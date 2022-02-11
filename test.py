9cd0f3b1a735e5b7534f44fd71153f5ca193dee7
talhar123456 <48639541+talhar123456@users.noreply.github.com>
2022-02-12 00:29:51 +0100

    cricket match

--- test.py
+++ test.py
@@ -1,6 +1,22 @@
-print('hello world')
+import random
 
-def add(x, y):
-    print (x + y)
+toss = input("Heads or Tails?")
+        
 
-add(2,3)
+list = ["heads", "tails"]
+randomToss = random.choice(list)
+
+if toss == randomToss:
+    print("you won the toss")
+
+else:
+    print("you lose the toss")
+
+
+# a = random.randint(1, 2)
+# print(a)
+
+# for i in range(0, 6):
+#         a = random.randint(1, 6)
+#         print(a)
+
